686504354b33b369a32e42a36aecc3f2ef19760f
Amarilllis <amarilllis@yandex.ru>
2016-05-06 21:43:43 +0400

    beginning to build the templates

--- lexical_templates.py
+++ lexical_templates.py
@@ -2,3 +2,16 @@ __author__ = 'amarilllis'
 
 import pickle
 import random
+
+def gen_template():
+    template = {}
+
+    template["root"] = ("pronoun","property")
+    template["person"] = ("verb_right","property")
+    # maybe no property here
+
+    template["verb_right"] = ("property",)
+    template["adjective"] = ("property",)
+    template["property"] = ("person",)
+
+    return template
