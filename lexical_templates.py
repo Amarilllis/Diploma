81813a2f477cd560edfc75eadbdec6bae7fde443
Amarilllis <amarilllis@yandex.ru>
2016-05-09 11:36:14 +0400

    more methods

--- lexical_templates.py
+++ lexical_templates.py
@@ -2,6 +2,7 @@ __author__ = 'amarilllis'
 
 import pickle
 import random
+import pymorphy2
 
 def gen_template():
     template = {}
@@ -18,6 +19,30 @@ def gen_template():
 def gen_review(template, words):
     review = ""
 
-    
+    cur = random.choice(template["root"])
+
+    while cur != "end":
+        next_word = random.choice(words[cur])
+        review += next_word + " "
+        cur = random.choice(template[cur])
 
     return review
+
+def gen_words():
+    words = {}
+
+    words["person"] = []
+    words["verb_right"] = []
+    words["verb_left"] = []
+    words["property"] = []
+    words["adjective"] = []
+    
+    return words
+
+template = gen_template()
+pickle.dump(template, open("template_v1.p", "wb"))
+
+'''
+template = pickle.load(open("template_v1.p", "rb"))
+
+'''
