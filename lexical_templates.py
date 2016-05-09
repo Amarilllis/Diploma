317b50430e6fbfa7c0c75044b5b5ce1860b5af1d
Amarilllis <amarilllis@yandex.ru>
2016-05-09 11:03:23 +0400

    more branches in the template

--- lexical_templates.py
+++ lexical_templates.py
@@ -6,12 +6,18 @@ import random
 def gen_template():
     template = {}
 
-    template["root"] = ("pronoun","property")
-    template["person"] = ("verb_right","property")
-    # maybe no property here
+    template["root"] = ("person", "property")
+    template["person"] = ("verb_right", "property")
 
-    template["verb_right"] = ("property",)
-    template["adjective"] = ("property",)
-    template["property"] = ("person",)
+    template["verb_right"] = ("property","end")
+    template["adjective"] = ("property", "end")
+    template["property"] = ("person", "adjective", "end")
 
     return template
+
+def gen_review(template, words):
+    review = ""
+
+    
+
+    return review
